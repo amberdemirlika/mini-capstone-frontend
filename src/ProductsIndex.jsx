92fe60cf041e52873289c6ceca904ba9545b65f1
amberdemirlika <amberdemir12@gmail.com>
2024-09-25 19:53:09 -0500

    add index action

---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,18 @@
+export function ProductsIndex(props) {
+  console.log(props.products);
+  return (
+    <div>
+      <h1> All Products</h1>
+      {props.products.map(product => (
+        <div key = {product.id}>
+          <p> {product.name}</p>
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+          <img src={product.images[0] && product.images[0].url} />
+          </div>
+
+
+      ))}
+    </div>
+  );
+}
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,7 +1,24 @@
-export function ProductsPage () {
+import {ProductsIndex} from './ProductsIndex'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+export function ProductsPage() {
+  const [products, setProducts] = useState([])
+
+  const handleProductsIndex = () => {
+    console.log('in products index')
+    axios.get("http://localhost:3000/products.json").then(response => {
+      console.log(response.data)
+      setProducts(response.data)
+    })
+  }
+
+  useEffect(handleProductsIndex, [])
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <ProductsIndex products={products}/>
     </main>
   )
 }
